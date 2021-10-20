832997bfc2339ba7a20501450eff8f3643aea9fb
dferre1997 <davide.ferre1997@gmail.com>
2021-10-20 09:41:47 +0200

    Simple graphical solution print function and boolean model first steps

--- src/main/java/BooleanModel.java
+++ src/main/java/BooleanModel.java
@@ -1,5 +1,51 @@
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.Solution;
+import org.chocosolver.solver.variables.BoolVar;
+import org.chocosolver.solver.variables.IntVar;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class BooleanModel implements TryYourStuff {
+    @Override
+    public void ferre() {
+        int n = 8;
+        Model model = new Model(n + "-queen problem boolean");
+        // Create a matrix of nxn bool variables
+        IntVar[][] matrix = model.intVarMatrix("c", n, n, 0, 1);
+
+        //constraints
+        IntVar[] flatArray = new IntVar[n*n];
+        for (int index = 0; index < n*n; index++) {
+            int i = index / n;
+            int j = index % n;
+            flatArray[index] = matrix[i][j];
+        }
+        model.sum(flatArray,"=",n).post(); // total number of queens constraint
+
+        for(int i = 0; i < n; i++) {
+            model.sum(matrix[i], "=", 1).post(); // 1 queen in each row constraint
+        }
+
+        IntVar[][] cols = new IntVar[n][n];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                cols[i][j] = matrix[j][i];
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            model.sum(cols[i], "=", 1).post(); // 1 queen in each column constraint
+        }
+
+        Solution solution = model.getSolver().findSolution();
+        if (solution != null) {
+            System.out.println(solution.toString());
+            Utilities.printMatrix(matrix, n);
+        }
+    }
+
     public static void main(String[] args) {
         BooleanModel m = new BooleanModel();
+        m.ferre();
     }
 }
---
+++ src/main/java/Utilities.java
@@ -0,0 +1,28 @@
+import org.chocosolver.solver.variables.IntVar;
+
+public final class Utilities {
+    /**
+     * Prints a IntVar matrix in a simple graphical form
+     * @param matrix if a cell=1 then it prints #
+     * @param n dimension of the square matrix
+     */
+    public static void printMatrix(IntVar matrix[][], int n) {
+        int[][] solved_matrix = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                solved_matrix[i][j] = matrix[i][j].getValue();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(solved_matrix[i][j] == 1) {
+                    System.out.print("# ");
+                } else {
+                    System.out.print("- ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
