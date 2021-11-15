619d4665dbb1048ddfab9afa9cc838e5ae1b9ec6
Alessandro <aless.pep@hotmail.fr>
2021-11-15 19:57:52 +0100

    Added week distance constraint and no empty week constraint

--- src/main/java/timetabling/IntegerModel.java
+++ src/main/java/timetabling/IntegerModel.java
@@ -3,8 +3,10 @@ package timetabling;
 import org.chocosolver.solver.Model;
 import org.chocosolver.solver.Solution;
 import org.chocosolver.solver.Solver;
+import org.chocosolver.solver.expression.discrete.arithmetic.ArExpression;
 import org.chocosolver.solver.expression.discrete.relational.ReExpression;
 import org.chocosolver.solver.variables.IntVar;
+import org.chocosolver.solver.variables.impl.BitsetIntVarImpl;
 
 public class IntegerModel {
 	Data data;
@@ -14,7 +16,9 @@ public class IntegerModel {
 	}
 
 	public static void main(String[] args) {
-		IntegerModel m = new IntegerModel( new Data(2, 3, 2, 2, 3));
+		IntegerModel m = new IntegerModel( new Data(4, 4, 2, 5, 4));
+
+		// Declaring model
 
 		Model model = new Model("Timetabling problem");
 		
@@ -29,6 +33,8 @@ public class IntegerModel {
 				T[i][j] = model.intVar("T"+i+","+j, 0, m.data.timesOfDay-1, false);
 			}
 		}
+
+		// Defining constraints
 		
 		int size = m.data.courses * m.data.lectures;
 		
@@ -45,11 +51,13 @@ public class IntegerModel {
 		model.allDifferent(timeslots).post();
 
 		int timeSlotperWeek = m.data.days * m.data.timesOfDay;
-		if (timeSlotperWeek >= m.data.courses && m.data.lectures <= m.data.weeks) {
+
+		// One lecture of each course per week
+		if (timeSlotperWeek >= m.data.courses && m.data.lectures == m.data.weeks) {
 			System.out.println("Case 1 ");
 			for (int i = 0; i < m.data.courses; i++) {
 				for (int j = 1; j < m.data.lectures; j++) {
-					// lectures of a course can't be in the same week,is in the same day and in the same timeofday
+					// lectures of a course can't be in the same week, is in the same day and in the same timeofday
 					model.arithm(W[i][j - 1], "<", W[i][j]).post();
 					model.arithm(D[i][j - 1], "=", D[i][j]).post();
 					model.arithm(T[i][j - 1], "=", T[i][j]).post();
@@ -61,16 +69,44 @@ public class IntegerModel {
 
 			System.out.println("Case 2 ");
 			// distance can vary depending on nb of lectures and nb of days
-			int dis = 1 ;
+			int disD = 1;
 			for (int i = 0; i < m.data.courses; i++) {
 				for (int j = 1; j < m.data.lectures; j++) {
-					// lectures of a course can be in the same week but different day (with a distance),
-					ReExpression y1 = W[i][j - 1].eq(W[i][j]).and(D[i][j].sub(D[i][j-1]).gt(dis)) ;
+					// lectures of a course can be in the same week but maximum <disD> days apart
+					ReExpression y1 = W[i][j - 1].eq(W[i][j]).and(D[i][j].sub(D[i][j-1]).gt(disD));
 					ReExpression y2 = W[i][j - 1].lt(W[i][j]).and(D[i][j - 1].eq(D[i][j]).and(T[i][j - 1].eq(T[i][j])));
 					y1.or(y2).post();
 				}
 			}
 		}
+
+
+		if (timeSlotperWeek >= m.data.courses && m.data.lectures < m.data.weeks) {
+			System.out.println("Case 3 ");
+			int disW = 1;
+			for (int i = 0; i < m.data.courses; i++) {
+				for (int j = 1; j < m.data.lectures; j++) {
+					// lectures of a course are maximum <disW> weeks apart
+					ArExpression diff = W[i][j].sub(W[i][j-1]);
+					ReExpression y1 = diff.le(disW).and(diff.gt(0)).and(D[i][j - 1].eq(D[i][j]).and(T[i][j - 1].eq(T[i][j])));
+					y1.post();
+				}
+			}
+		}
+
+		IntVar[] flatW = model.intVarArray("F", size,0 , m.data.weeks-1, false);
+
+		for (int i = 0; i < m.data.courses; i++) {
+			for (int j = 0; j < m.data.lectures; j++) {
+				flatW[i*m.data.lectures+j].eq(W[i][j]).post();
+			}
+		}
+
+		// No empty week
+		IntVar diffValues = model.intVar(m.data.weeks);
+		model.nValues(flatW, diffValues).post();
+
+		// Solving
 
 		Solver solver = model.getSolver();
 		int i = 1;
@@ -83,9 +119,9 @@ public class IntegerModel {
 		/*Solution solution = solver.findSolution();
 
 		if(solution != null) {
-			//m.printSolution(W,D,T);
+			m.printSolution(W,D,T);
 			for (int j = 0; j < size; j++) {
-				System.out.println(timeslots[j]);
+				System.out.println(flatW[j]);
 			}
 		}*/
 	}
