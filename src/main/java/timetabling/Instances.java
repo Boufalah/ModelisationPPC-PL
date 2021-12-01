73756a5908c23b6fbe2dc70249cd23094eec4efb
Alessandro <aless.pep@hotmail.fr>
2021-12-01 20:28:59 +0100

    Added instances file

---
+++ src/main/java/timetabling/Instances.java
@@ -0,0 +1,19 @@
+package timetabling;
+
+ public class Instances {
+	 
+	//w,d,t,c,l 
+	
+	static Data small1 = new Data(2,3,2,2,2);
+	
+	static Data small2 = new Data(3,3,2,3,3);
+	
+	static Data medium1 = new Data(6,4,2,4,4);
+	
+	static Data medium2 = new Data(8,4,2,5,5);
+	
+	static Data large1 = new Data(10,5,2,6,6);
+	
+	static Data large2 = new Data(12,5,2,7,7);
+	
+}
